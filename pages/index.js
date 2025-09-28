856b6d480330826ac9b86419b96e04aafd4651d1
LittleCodr <mayankheroagrawal25@gmail.com>
2025-09-28 12:58:41 +0530

    Made GA Conditional

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Ubuntu from "../components/ubuntu";
 import ReactGA from 'react-ga4';
 import Meta from "../components/SEO/Meta";
 
+// Only initialize GA in production or if TRACKING_ID is provided
 const TRACKING_ID = process.env.NEXT_PUBLIC_TRACKING_ID;
-ReactGA.initialize(TRACKING_ID);
+if (typeof window !== 'undefined' && TRACKING_ID) {
+  ReactGA.initialize(TRACKING_ID);
+}
 
 function App() {
   return (
