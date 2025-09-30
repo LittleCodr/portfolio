01cf7fbee8f12d3d002e4a5a01df4d3d3485ba9d
LittleCodr <mayankheroagrawal25@gmail.com>
2025-09-30 11:37:10 +0530

    Fixed Hydration Error

--- pages/_document.js
+++ pages/_document.js
@@ -10,18 +10,6 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                    {/* Google tag (gtag.js) */}
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0EEBPFMJN4"></script>
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
-                                window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
-                                gtag('config', 'G-0EEBPFMJN4');
-                            `,
-                        }}
-                    />
                 </Head>
                 <body>
                     <Main />
